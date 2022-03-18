8969a06a2dc888c3ccc20aa4b0f38bfcce1c838b
Paolo Possanzini <paolo@teamdev.it>
2022-03-18 11:42:46 +0100

    configuration extensions

--- arbitrer/extensions/ArbitrerExtensions.cs
+++ arbitrer/extensions/ArbitrerExtensions.cs
@@ -1,5 +1,8 @@
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -27,6 +30,42 @@ namespace Arbitrer
     public static ArbitrerOptions SetAsRemoteRequest<T>(this ArbitrerOptions options) where T : IBaseRequest
     {
       options.RemoteRequests.Add(typeof(T));
+      return options;
+    }
+
+    public static ArbitrerOptions SetAsLocalRequests(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect)
+    {
+      var types = (from a in assemblySelect()
+                   from t in a.GetTypes()
+                   where typeof(IBaseRequest).IsAssignableFrom(t)
+                   select t).AsEnumerable();
+      foreach (var t in types)
+        options.LocalRequests.Add(t);
+      return options;
+    }
+
+    public static ArbitrerOptions SetAsLocalRequests(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect)
+    {
+      foreach (var t in typesSelect())
+        options.LocalRequests.Add(t);
+      return options;
+    }
+
+    public static ArbitrerOptions SetAsRemoteRequests(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect)
+    {
+      var types = (from a in assemblySelect()
+                   from t in a.GetTypes()
+                   where typeof(IBaseRequest).IsAssignableFrom(t)
+                   select t).AsEnumerable();
+      foreach (var t in types)
+        options.RemoteRequests.Add(t);
+      return options;
+    }
+
+    public static ArbitrerOptions SetAsRemoteRequests(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect)
+    {
+      foreach (var t in typesSelect())
+        options.RemoteRequests.Add(t);
       return options;
     }
   }
