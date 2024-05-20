3f89bce49294c3f6e0535ac7d3d562e23d8b29ef
Paolo Possanzini <paolo@teamdev.it>
2024-05-20 10:13:37 +0200

    client name

--- Arbitrer.RabbitMQ/RequestsManager.cs
+++ Arbitrer.RabbitMQ/RequestsManager.cs
@@ -100,6 +100,7 @@ namespace Arbitrer.RabbitMQ
           VirtualHost = _options.VirtualHost,
           Port = _options.Port,
           DispatchConsumersAsync = true,
+          ClientProvidedName = _options.ClientName
         };
 
         _connection = factory.CreateConnection();
