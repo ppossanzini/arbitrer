10b521e349456119c35721cd89b694bf1ab10c05
Paolo Possanzini <paolo@teamdev.it>
2024-07-11 14:08:55 +0200

    added logging during configurations

--- Arbitrer/extensions/ArbitrerExtensions.cs
+++ Arbitrer/extensions/ArbitrerExtensions.cs
@@ -7,6 +7,7 @@ using System.Text;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Arbitrer
@@ -57,8 +58,11 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The existing arbitrer options.</param>
     /// <param name="assemblies">The assemblies to search for request handlers.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The updated arbitrer options with the inferred local requests.</returns>
-    public static ArbitrerOptions InferLocalRequests(this ArbitrerOptions options, IEnumerable<Assembly> assemblies, string queuePrefix = null)
+    public static ArbitrerOptions InferLocalRequests(this ArbitrerOptions options, IEnumerable<Assembly> assemblies, string queuePrefix = null,
+      ILogger logger = null)
     {
       var localRequests = assemblies.SelectMany(a => a
         .GetTypes()
@@ -66,7 +70,7 @@ namespace Arbitrer
           .Where(i => i.FullName != null && i.FullName.StartsWith("MediatR.IRequestHandler"))
           .Select(i => i.GetGenericArguments()[0]).ToArray()
         ));
-      options.SetAsLocalRequests(localRequests.ToArray, queuePrefix);
+      options.SetAsLocalRequests(localRequests.ToArray, queuePrefix, logger);
       return options;
     }
 
@@ -76,10 +80,13 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The <see cref="ArbitrerOptions"/> object.</param>
     /// <param name="assemblies">The collection of <see cref="Assembly"/> objects to infer local notifications from.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>
     /// The <see cref="ArbitrerOptions"/> object with inferred local notifications set.
     /// </returns>
-    public static ArbitrerOptions InferLocalNotifications(this ArbitrerOptions options, IEnumerable<Assembly> assemblies, string queuePrefix = null)
+    public static ArbitrerOptions InferLocalNotifications(this ArbitrerOptions options, IEnumerable<Assembly> assemblies, string queuePrefix = null,
+      ILogger logger = null)
     {
       var localNotifications = assemblies.SelectMany(a => a
         .GetTypes()
@@ -88,7 +95,8 @@ namespace Arbitrer
           .Select(i => i.GetGenericArguments()[0]).ToArray()
         ));
 
-      options.SetAsLocalRequests(() => localNotifications, queuePrefix);
+      options.SetAsLocalRequests(() => localNotifications, queuePrefix, logger);
+
       return options;
     }
 
@@ -97,25 +105,39 @@ namespace Arbitrer
     /// </summary>
     /// <typeparam name="T">The type of the request to set as local.</typeparam>
     /// <param name="options">The ArbitrerOptions object to modify.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The modified ArbitrerOptions object.</returns>
-    public static ArbitrerOptions SetAsLocalRequest<T>(this ArbitrerOptions options, string queuePrefix = null) where T : IBaseRequest
+    public static ArbitrerOptions SetAsLocalRequest<T>(this ArbitrerOptions options, string queuePrefix = null, ILogger logger = null) where T : IBaseRequest
     {
       options.LocalRequests.Add(typeof(T));
 
-      if (!string.IsNullOrWhiteSpace(queuePrefix) && !options.QueuePrefixes.ContainsKey(typeof(T).FullName))
-        options.QueuePrefixes.Add(typeof(T).FullName, queuePrefix);
+      if (!string.IsNullOrWhiteSpace(queuePrefix) && !options.QueuePrefixes.ContainsKey(typeof(T).FullName!))
+      {
+        options.QueuePrefixes.Add(typeof(T).FullName!, queuePrefix);
+        logger?.LogInformation($"Added prefix to request ${typeof(T).FullName}");
+      }
+
       return options;
     }
 
     /// <summary>
-    /// Listens for a notification and adds it to the local requests list in the ArbitrerOptions instance. </summary> <typeparam name="T">The type of the notification to listen for. It must implement the INotification interface.</typeparam> <param name="options">The ArbitrerOptions instance to add the notification to.</param> <returns>The updated ArbitrerOptions instance with the notification added to the local requests list.</returns>
+    /// Listens for a notification and adds it to the local requests list in the ArbitrerOptions instance. </summary> <typeparam name="T">The type of the notification to listen for. It must implement the INotification interface.</typeparam> <param name="options">The ArbitrerOptions instance to add the notification to.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
+    /// <returns>The updated ArbitrerOptions instance with the notification added to the local requests list.</returns>
     /// /
-    public static ArbitrerOptions ListenForNotification<T>(this ArbitrerOptions options, string queuePrefix = null) where T : INotification
+    public static ArbitrerOptions ListenForNotification<T>(this ArbitrerOptions options, string queuePrefix = null, ILogger logger = null)
+      where T : INotification
     {
       options.LocalRequests.Add(typeof(T));
 
-      if (!string.IsNullOrWhiteSpace(queuePrefix) && !options.QueuePrefixes.ContainsKey(typeof(T).FullName))
-        options.QueuePrefixes.Add(typeof(T).FullName, queuePrefix);
+      if (!string.IsNullOrWhiteSpace(queuePrefix) && !options.QueuePrefixes.ContainsKey(typeof(T).FullName!))
+      {
+        options.QueuePrefixes.Add(typeof(T).FullName!, queuePrefix);
+        logger?.LogInformation($"Added prefix to request ${typeof(T).FullName}");
+      }
+
       return options;
     }
 
@@ -124,13 +146,19 @@ namespace Arbitrer
     /// </summary>
     /// <typeparam name="T">The type of the remote request. It must implement the <see cref="IBaseRequest"/> interface.</typeparam>
     /// <param name="options">The <see cref="ArbitrerOptions"/> instance to modify.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The modified <see cref="ArbitrerOptions"/> instance.</returns>
-    public static ArbitrerOptions SetAsRemoteRequest<T>(this ArbitrerOptions options, string queuePrefix = null) where T : IBaseRequest
+    public static ArbitrerOptions SetAsRemoteRequest<T>(this ArbitrerOptions options, string queuePrefix = null, ILogger logger = null) where T : IBaseRequest
     {
       options.RemoteRequests.Add(typeof(T));
 
-      if (!string.IsNullOrWhiteSpace(queuePrefix) && !options.QueuePrefixes.ContainsKey(typeof(T).FullName))
-        options.QueuePrefixes.Add(typeof(T).FullName, queuePrefix);
+      if (!string.IsNullOrWhiteSpace(queuePrefix) && !options.QueuePrefixes.ContainsKey(typeof(T).FullName!))
+      {
+        options.QueuePrefixes.Add(typeof(T).FullName!, queuePrefix);
+        logger?.LogInformation($"Added prefix to request ${typeof(T).FullName}");
+      }
+
 
       return options;
     }
@@ -140,8 +168,11 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The <see cref="ArbitrerOptions"/> to modify.</param>
     /// <param name="assemblySelect">A function that selects the assemblies to retrieve types from.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The modified <see cref="ArbitrerOptions"/>.</returns>
-    public static ArbitrerOptions SetAsLocalRequests(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect, string queuePrefix = null)
+    public static ArbitrerOptions SetAsLocalRequests(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect, string queuePrefix = null,
+      ILogger logger = null)
     {
       var types = (from a in assemblySelect()
         from t in a.GetTypes()
@@ -153,8 +184,11 @@ namespace Arbitrer
 
       if (!string.IsNullOrWhiteSpace(queuePrefix))
         foreach (var t in types)
-          if (!options.QueuePrefixes.ContainsKey(t.FullName))
+          if (!options.QueuePrefixes.ContainsKey(t.FullName!))
+          {
             options.QueuePrefixes.Add(t.FullName, queuePrefix);
+            logger?.LogInformation($"Added prefix to request ${t.FullName}");
+          }
 
       return options;
     }
@@ -164,16 +198,22 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The <see cref="ArbitrerOptions"/> object.</param>
     /// <param name="typesSelect">A function that returns an enumerable collection of types to be set as local requests.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The updated <see cref="ArbitrerOptions"/> object.</returns>
-    public static ArbitrerOptions SetAsLocalRequests(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect, string queuePrefix = null)
+    public static ArbitrerOptions SetAsLocalRequests(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect, string queuePrefix = null,
+      ILogger logger = null)
     {
       foreach (var t in typesSelect())
         options.LocalRequests.Add(t);
 
       if (!string.IsNullOrWhiteSpace(queuePrefix))
         foreach (var t in typesSelect())
-          if (!options.QueuePrefixes.ContainsKey(t.FullName))
+          if (!options.QueuePrefixes.ContainsKey(t.FullName!))
+          {
             options.QueuePrefixes.Add(t.FullName, queuePrefix);
+            logger?.LogInformation($"Added prefix to request ${t.FullName}");
+          }
 
       return options;
     }
@@ -183,8 +223,11 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The <see cref="ArbitrerOptions"/> to set as remote requests.</param>
     /// <param name="assemblySelect">The function to select the assemblies.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The updated <see cref="ArbitrerOptions"/> with remote requests set.</returns>
-    public static ArbitrerOptions SetAsRemoteRequests(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect, string queuePrefix = null)
+    public static ArbitrerOptions SetAsRemoteRequests(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect, string queuePrefix = null,
+      ILogger logger = null)
     {
       var types = (from a in assemblySelect()
         from t in a.GetTypes()
@@ -195,8 +238,12 @@ namespace Arbitrer
 
       if (!string.IsNullOrWhiteSpace(queuePrefix))
         foreach (var t in types)
-          if (!options.QueuePrefixes.ContainsKey(t.FullName))
+          if (!options.QueuePrefixes.ContainsKey(t.FullName!))
+          {
             options.QueuePrefixes.Add(t.FullName, queuePrefix);
+            logger?.LogInformation($"Added prefix to request ${t.FullName}");
+          }
+
       return options;
     }
 
@@ -205,16 +252,23 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The ArbitrerOptions object.</param>
     /// <param name="typesSelect">The function that returns IEnumerable of Type objects.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for requests</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The modified ArbitrerOptions object.</returns>
-    public static ArbitrerOptions SetAsRemoteRequests(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect, string queuePrefix = null)
+    public static ArbitrerOptions SetAsRemoteRequests(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect, string queuePrefix = null,
+      ILogger logger = null)
     {
       foreach (var t in typesSelect())
         options.RemoteRequests.Add(t);
 
       if (!string.IsNullOrWhiteSpace(queuePrefix))
         foreach (var t in typesSelect())
-          if (!options.QueuePrefixes.ContainsKey(t.FullName))
+          if (!options.QueuePrefixes.ContainsKey(t.FullName!))
+          {
             options.QueuePrefixes.Add(t.FullName, queuePrefix);
+            logger?.LogInformation($"Added prefix to request ${t.FullName}");
+          }
+
       return options;
     }
 
@@ -223,13 +277,20 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The ArbitrerOptions object.</param>
     /// <param name="typesSelect">The function that returns IEnumerable of Type objects.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for notification</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The modified ArbitrerOptions object.</returns>
-    public static ArbitrerOptions SetNotificationPrefix(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect, string queuePrefix)
+    public static ArbitrerOptions SetNotificationPrefix(this ArbitrerOptions options, Func<IEnumerable<Type>> typesSelect, string queuePrefix,
+      ILogger logger = null)
     {
       if (!string.IsNullOrWhiteSpace(queuePrefix))
         foreach (var t in typesSelect().Where(t => typeof(INotification).IsAssignableFrom(t)))
-          if (!options.QueuePrefixes.ContainsKey(t.FullName))
+          if (!options.QueuePrefixes.ContainsKey(t.FullName!))
+          {
             options.QueuePrefixes.Add(t.FullName, queuePrefix);
+            logger?.LogInformation($"Added prefix to notification ${t.FullName}");
+          }
+
       return options;
     }
 
@@ -238,8 +299,11 @@ namespace Arbitrer
     /// </summary>
     /// <param name="options">The ArbitrerOptions object.</param>
     /// <param name="assemblySelect">The function to select the assemblies.</param>
+    /// <param name="queuePrefix">Prefix for Exchange and queues for notification</param>
+    /// <param name="logger">Logger instance to allow information during configuration</param>
     /// <returns>The modified ArbitrerOptions object.</returns>
-    public static ArbitrerOptions SetNotificationPrefix(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect, string queuePrefix)
+    public static ArbitrerOptions SetNotificationPrefix(this ArbitrerOptions options, Func<IEnumerable<Assembly>> assemblySelect, string queuePrefix,
+      ILogger logger = null)
     {
       var types = (from a in assemblySelect()
         from t in a.GetTypes()
@@ -247,8 +311,12 @@ namespace Arbitrer
         select t).AsEnumerable();
 
       foreach (var t in types)
-        if (!options.QueuePrefixes.ContainsKey(t.FullName))
+        if (!options.QueuePrefixes.ContainsKey(t.FullName!))
+        {
           options.QueuePrefixes.Add(t.FullName, queuePrefix);
+          logger?.LogInformation($"Added prefix to notification ${t.FullName}");
+        }
+
       return options;
     }
 
@@ -267,7 +335,7 @@ namespace Arbitrer
       }
 
       // var prefix = options.DefaultQueuePrefix;
-      options.QueuePrefixes.TryGetValue(t.FullName, out var prefix);
+      options.QueuePrefixes.TryGetValue(t.FullName!, out var prefix);
       prefix = prefix ?? options.DefaultQueuePrefix;
 
       sb = sb ?? new StringBuilder();
@@ -275,7 +343,7 @@ namespace Arbitrer
       if (!string.IsNullOrWhiteSpace(prefix)) sb.Append($"{prefix}.");
       sb.Append($"{t.Namespace}.{t.Name}");
 
-      if (t.GenericTypeArguments != null && t.GenericTypeArguments.Length > 0)
+      if (t.GenericTypeArguments is { Length: > 0 })
       {
         sb.Append("[");
         foreach (var ta in t.GenericTypeArguments)
