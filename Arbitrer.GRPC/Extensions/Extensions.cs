a6504fab68297b0159070d9dfd67389328a608b0
Paolo Possanzini <paolo@teamdev.it>
2024-09-25 17:26:16 +0200

    Added GRPC over HTTP2

--- Arbitrer.GRPC/Extensions/Extensions.cs
+++ Arbitrer.GRPC/Extensions/Extensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using Arbitrer.GRPC;
+using Grpc.AspNetCore.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
@@ -10,26 +11,57 @@ using Microsoft.Extensions.Hosting;
 
 namespace Arbitrer.GRPC.Extensions
 {
+  
+  
   public static class Extensions
   {
+    
+    const string ArbitrerGrpcCorsDefaultPolicy = "ArbitrerGRPCDefault";
+    
     /// <summary>
     /// Add the Arbitrer RabbitMQ message dispatcher to the service collection, allowing it to be resolved and used.
     /// </summary>
     /// <param name="services">The service collection to add the message dispatcher to.</param>
     /// <param name="config">The configuration settings for the message dispatcher.</param>
+    /// <param name="grpcOptions">Grpc service configuration options, if not specified you need to call AddGrpc()
+    /// registration method.</param>
     /// <returns>The updated service collection.</returns>
-    public static IServiceCollection AddArbitrerGrpcDispatcher(this IServiceCollection services, Action<MessageDispatcherOptions> config)
+    public static IServiceCollection AddArbitrerGrpcDispatcher(this IServiceCollection services, 
+      Action<MessageDispatcherOptions> config, 
+      Action<GrpcServiceOptions> grpcOptions = null)
     {
-      services.AddGrpc();
+
+      if (grpcOptions != null)
+      {
+        services.Configure(grpcOptions);
+        services.AddGrpc();
+      }
+
+      services.AddCors(o => o.AddPolicy(ArbitrerGrpcCorsDefaultPolicy, builder =>
+      {
+        builder.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader()
+          .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+      }));
+      
       services.Configure<MessageDispatcherOptions>(config);
       services.AddSingleton<IExternalMessageDispatcher, MessageDispatcher>();
 
       return services;
     }
-
+    
     public static IEndpointRouteBuilder UseGrpcDispatcher(this IEndpointRouteBuilder host)
     {
       host.MapGrpcService<RequestsManager>();
+      return host;
+    }
+
+    public static IEndpointRouteBuilder UseGrpcWebDispatcher(this IEndpointRouteBuilder host)
+    {
+      host.MapGrpcService<RequestsManager>()
+        .EnableGrpcWeb()
+        .RequireCors(ArbitrerGrpcCorsDefaultPolicy);
       return host;
     }
 
